d82be6e81c310d59db7512ad6a03020d9bdab077
Anil Singh <anil.singh581@gmail.com>
2024-08-28 01:48:21 +0530

    Update DatabaseContext.cs

--- DatabaseContext.cs
+++ DatabaseContext.cs
@@ -1,19 +1,1 @@
-﻿using CodeFirstApproach.Entities;
-using Microsoft.EntityFrameworkCore;
-using System.Data;
 
-namespace CodeFirstApproach.DatabaseContext
-{
-    public class DatabaseContext:DbContext
-    {
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Item> Items { get; set; }
-        public DbSet<OrderItem> OrderItems { get; set; }
-        public DbSet<Order> Orders { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=SQLTST;Initial Catalog=cfoTest;Persist Security Info=True; User Id=sa;Password=TTTTTTT;Integrated Security=True; Trusted_Connection=False; TrustServerCertificate=True; Encrypt=True;");
-        }
-    }
-}
